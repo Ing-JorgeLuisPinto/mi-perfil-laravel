483ff273339bf83a3d89a6320355aea358e7b7a1
Jorge Pinto <pintojorgeluis16@gmail.com>
2026-02-14 15:33:23 -0500

    Vista intereses.blade.php creada

---
+++ resources/views/intereses.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Intereses</title>
+</head>
+<body>
+    <h1>Mis Intereses</h1>
+    <ul>
+        <li>Programación</li>
+        <li>Tecnología</li>
+        <li>Videojuegos</li>
+        <li>Musica</li>
+        <li>Carros</li>
+
+
+    </ul>
+</body>
+</html>
