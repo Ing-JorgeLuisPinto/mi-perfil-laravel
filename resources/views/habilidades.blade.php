714bb46823f7ec30aac24668d9d41b7a9a75f5b5
Jorge Pinto <pintojorgeluis16@gmail.com>
2026-02-14 15:36:13 -0500

    Vista habilidades.blade.php creada

---
+++ resources/views/habilidades.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Habilidades</title>
+</head>
+<body>
+    <h1>Habilidades Técnicas</h1>
+    <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>PHP</li>
+        <li>PYTHON</li>
+        <li>KOTLIN</li>
+        <li>JAVA</li>
+        <li>JAVASCRIPT</li>
+
+
+    </ul>
+</body>
+</html>
