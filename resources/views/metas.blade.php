d8b0f82944da9ede83957a5d05185920f2f169bf
Jorge Pinto <pintojorgeluis16@gmail.com>
2026-02-14 15:42:15 -0500

    Vista metas.blade.php creada

---
+++ resources/views/metas.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Metas</title>
+</head>
+<body>
+    <h1>Metas Profesionales</h1>
+    <p>Acabar la carrera</p>
+    <p>Conseguir un buen trabajo</p>
+    <p>Poder trabajar para otro pais o en otro pais </p>
+
+
+
+    <h1>Metas Personales</h1>
+    <p>Tener una familia</p>
+    <p>Comprarme la casa de mi sueños </p>
+    <p>Comprarme las motos de mi sueños </p>
+    <p>Comprarme los carros de mi sueños </p>
+
+
+
+
+</body>
+</html>
